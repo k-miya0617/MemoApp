978d63282cc1184b8dcad77c875c5ca2c8f823bc
K.Miya <51694607+k-miya0617@users.noreply.github.com>
2021-12-10 22:40:05 +0900

    #74 done. make markup login screen.

---
+++ src/screens/LogInScreen.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  View, Text, TextInput, StyleSheet,
+} from 'react-native';
+
+import AppBar from '../components/AppBar';
+
+export default function LogInScreen() {
+  return (
+    <View style={styles.container}>
+      <AppBar />
+      <View>
+        <Text>Log In</Text>
+        <TextInput value="Email Address" />
+        <TextInput value="Password" />
+        <View>
+          <Text>Submit</Text>
+        </View>
+        <View>
+          <Text>Not registered?</Text>
+          <Text>Sign up here!</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+  },
+});
