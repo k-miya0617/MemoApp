90c5dedd3ae44ab22271f83c4b049779420440b3
K.Miya <51694607+k-miya0617@users.noreply.github.com>
2021-12-22 20:29:50 +0900

    #107 done. fix codes.

--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -41,8 +41,7 @@ export default function MemoListScreen(props) {
         });
         setMemos(userMemos);
         setLoading(false);
-      }, (error) => {
-        console.log(error.code);
+      }, () => {
         setLoading(false);
         Alert.alert('データの読み込みに失敗しました');
       });
